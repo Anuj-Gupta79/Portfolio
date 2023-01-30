f5df09df052a7fbdfdab3dcf2010705294c1eb4a
Anuj-Gupta79 <anujgupta7906@gmail.com>
2023-01-30 12:50:40 +0530

    set up

---
+++ portfoliofrontend/src/containers/Skill/skill.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './skill.scss'
+
+const skill = () => {
+  return (
+    <div>skill</div>
+  )
+}
+
+export default skill
---
+++ portfoliofrontend/src/containers/Testimoniols/testimoniols.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './testimoniols.scss'
+const testimoniols = () => {
+  return (
+    <div>testimoniols</div>
+  )
+}
+
+export default testimoniols
