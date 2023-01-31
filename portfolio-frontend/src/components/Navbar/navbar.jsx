2f1a0088dd2aea90caf30ba3daf630b3056b524c
Anuj-Gupta79 <anujgupta7906@gmail.com>
2023-01-31 22:14:29 +0530

    initial

--- portfolio-frontend/src/components/Navbar/navbar.jsx
+++ portfolio-frontend/src/components/Navbar/navbar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { images } from '../../constants';
 
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className="app__navbar">
+        <div className="app__navbar-logo">
+            <img src={images.logo} alt="logo"/>
+        </div>
+        <ul className="app__navbar-link">
+            {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                <li key={`link-${item}`}>
+                    <div className="app__flex p-text"/>
+                    <a href={`#${item}`}>{item}</a>
+                </li>
+            ))}
+        </ul>
+    </nav>
   )
 }
 
